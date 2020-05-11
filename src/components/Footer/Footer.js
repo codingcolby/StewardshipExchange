4974ef3654c13febba17ad9fe318d78ec455a9ce
allthethings <learn2code@respectdesfonds.com>
2020-05-11 16:26:46 -0500

    initial code base with enhancements from preliminary development - all with SE prefix

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+// This is one of our simplest components
+// It doesn't have local state, so it can be a function component.
+// It doesn't dispatch any redux actions or display any part of redux state
+// or even care what the redux state is, so it doesn't need 'connect()'
+
+const Footer = () => (
+	// <footer>
+	//   &copy; Prime Digital Academy
+	// </footer>
+	<footer>
+		&copy; Stewardship Exchange ||{" "}
+		<a href="mailto:techissues@stewardshipexchange.com">
+			techissues@stewardshipexchange.com
+		</a>
+	</footer>
+);
+
+export default Footer;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+
+const Nav = (props) => {
+	let loginLinkData = {
+		path: "/home",
+		text: "Login / Register",
+	};
+
+	if (props.store.user.id != null) {
+		loginLinkData.path = "/admin";
+		loginLinkData.text = "Home";
+	}
+
+	return (
+		<div className="nav">
+			{/* <Link to="/home"> */}
+			<img src="/archivesstorage.jpg" alt="storage" />
+			<h2 className="nav-title">The Stewardship Exchange</h2>
+			<p>
+				the <em>first</em> online resource for cultural heritage agencies to
+				share surplus preservation and conservation material for collections
+				care
+			</p>
+			{/* </Link> */}
+			{/* Always show these links since these pages are not protected */}
+			<div className="nav-right">
+				<Link className="nav-link" to="/home">
+					Home
+				</Link>
+				<Link className="nav-link" to="/works">
+					How It Works
+				</Link>
+				<Link className="nav-link" to="/stories">
+					Exchange Stories
+				</Link>
+				<Link className="nav-link" to="/about">
+					About / Contact
+				</Link>
+
+				{/* <Link className="nav-link" to="/about">
+					About
+				</Link> */}
+				<Link className="nav-link" to={loginLinkData.path}>
+					{/* Show this link if they are logged in or not,
+          but call this link 'Home' if they are logged in,
+          and call this link 'Login / Register' if they are not */}
+					{loginLinkData.text}
+				</Link>
+				{/* Show the link to the info page and the logout button if the user is logged in */}
+				{props.store.user.id && (
+					<>
+						<Link className="nav-link" to="/info">
+							Info Page
+						</Link>
+						<LogOutButton className="nav-link" />
+					</>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default connect(mapStoreToProps)(Nav);
