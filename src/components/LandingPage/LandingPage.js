7a8c3c1c450f59eb68dc8a1718c32a32054308b5
allthethings <learn2code@respectdesfonds.com>
2020-05-12 23:13:05 -0500

    added two different grids, one for static content and the other for displaying data from the server; added the search input field and button

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,51 +1,103 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
-
+import "../pageview.css";
 import "./LandingPage.css";
 
 class LandingPage extends Component {
 	state = {
-		heading: "Welcome!",
+		searchterm: "",
 	};
 
 	onLogin = (event) => {
 		this.props.history.push("/login");
 	};
 
+	handleSearchSubmit = (propertyName) => (event) => {
+		this.setState({
+			[propertyName]: event.target.value,
+		});
+	};
+
 	render() {
 		return (
 			<div className="container">
-				<h2>{this.state.heading}</h2>
+				<div className="pageText">
+					<h2>Welcome!</h2>
 
-				<div className="grid">
-					<div className="grid-col grid-col_1">
-						<h2>Goal 1</h2>
-						<p>Some text goes here</p>
+					<div className="grid">
+						<div className="grid-col grid-col_1">
+							<h2>Goal 1</h2>
+							<p>Some text goes here</p>
+						</div>
+
+						<div className="grid-col grid-col_2"></div>
+
+						<div className="grid-col grid-col_3">
+							<h2>Goal 2</h2>
+							<p>Some text goes here</p>
+						</div>
+
+						<div className="grid-col grid-col_4"></div>
+
+						<div className="grid-col grid-col_5">
+							<h2>Goal 3</h2>
+							<p>Some text goes here</p>
+						</div>
 					</div>
+				</div>
 
-					<div className="grid-col grid-col_2"></div>
+				<div className="pageText2">
+					<form>
+						<div>
+							<input
+								className="searchInput"
+								type="text"
+								name="searchterm"
+								value={this.state.searchterm}
+								placeholder="Enter term or keyword here"
+							/>
 
-					<div className="grid-col grid-col_3">
-						<h2>Goal 2</h2>
-						<p>Some text goes here</p>
+							<button
+								className="button"
+								type="submit"
+								name="searchtermbtn"
+								onChange={this.handleSearchSubmit("search")}>
+								SEARCH
+							</button>
+							<p className="current_regnow">Some text here</p>
+						</div>
+					</form>
+					<div className="datagrid">
+						<div className="datagrid-col datagrid-col_1">
+							<h4>Status</h4>
+							<div className="datagrid-row_1">
+								<p className="serverData">Server Data Here</p>
+								<p className="serverData">Server Data Here</p>
+							</div>
+						</div>
+						<div className="datagrid-col datagrid-col_2">
+							<h4>Date Posted</h4>
+							<div className="datagrid-row_1">
+								<p className="serverData">Server Data Here</p>
+								<p className="serverData">Server Data Here</p>
+							</div>
+						</div>
+						<div className="datagrid-col datagrid-col_3">
+							<h4>Location</h4>
+							<div className="datagrid-row_1">
+								<p className="serverData">Server Data Here</p>
+								<p className="serverData">Server Data Here</p>
+							</div>
+						</div>
+						<div className="datagrid-col datagrid-col_4">
+							<h4>Items Offered</h4>
+							<div className="datagrid-row_1">
+								<p className="serverData">Server Data Here</p>
+								<p className="serverData">Server Data Here</p>
+							</div>
+						</div>{" "}
 					</div>
-					<div className="grid-col grid-col_4"></div>
-
-					<div className="grid-col grid-col_5">
-						<h2>Goal 3</h2>
-						<p>Some text goes here</p>
-					</div>
-
-					{/* <div className="grid-col grid-col_4">
-                        <h3>Already a Member?</h3>
-                        <button
-                            className="btn btn_sizeFull"
-                            onClick={this.onLogin}
-                        >
-                            Login
-                        </button>
-                    </div> */}
 				</div>
 			</div>
 		);
