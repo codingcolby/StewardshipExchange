c625f5fffdd02691d22732417e7e4cb915773982
allthethings <learn2code@respectdesfonds.com>
2020-05-13 15:58:37 -0500

    removed unused code and comments

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -90,16 +90,6 @@ class LoginPage extends Component {
 						/>
 					</center>
 				</form>
-				{/* <center className="actionlinks">
-					{/* <button
-						type="button"
-						className="link-button"
-						onClick={() => {
-							this.props.dispatch({ type: "SET_TO_REGISTER_MODE" });
-						}}>
-						Register
-					</button> 
-				</center> */}
 			</div>
 		);
 	}
