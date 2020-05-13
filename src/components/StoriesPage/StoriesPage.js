4609d8d07ee122f45cb82a0991497b60aeabdcc4
allthethings <learn2code@respectdesfonds.com>
2020-05-12 23:08:45 -0500

    working with grids and styling to create the stories boxes that will receive server data - visual only at this time, not hooked up yet

--- src/components/StoriesPage/StoriesPage.js
+++ src/components/StoriesPage/StoriesPage.js
@@ -8,7 +8,88 @@ import "../pageview.css";
 
 const StoriesPage = () => (
 	<div className="container">
-		<p>Stories Page</p>
+		<div className="pageText">
+			<h2>Exchange Stories - time to celebrate!</h2>
+			<p>
+				Creating community is part of our mission. We invite you to share your
+				exchange experience - regardless if your exchange was big or small, or
+				whether you are an offering or receiving agency. Please{" "}
+				<button
+					type="button"
+					className="dk-link-button"
+					onClick={() => {
+						this.props.dispatch({ type: "SET_TO_LOGIN_MODE" });
+					}}>
+					Login
+				</button>{" "}
+				and submit a story from your page.
+			</p>
+		</div>
+		<div className="storygrid">
+			<div className="storygrid-col storygrid-col_1">
+				<p>
+					<h2>Story 1</h2>
+					Story Content Goes Here Story Content Goes Here Story Content Goes
+					Here Story Content Goes Here Story Content Goes Here Story Content
+					Goes Here Story Content Goes Here Story Content Goes Here
+				</p>
+				<div className="storygrid-row_1">
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+				</div>
+			</div>
+
+			<div className="storygrid-col storygrid-col_2">
+				<p>
+					<h2>Story 2</h2>
+					Story Content Goes Here Story Content Goes Here Story Content Goes
+					Here Story Content Goes Here Story Content Goes Here Story Content
+					Goes Here Story Content Goes Here Story Content Goes Here
+				</p>
+				<div className="storygrid-col storygrid-col_3">
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+				</div>
+			</div>
+
+			<div className="storygrid-col storygrid-col_2">
+				<p>
+					<h2>Story 3</h2>
+					Story Content Goes Here Story Content Goes Here Story Content Goes
+					Here Story Content Goes Here Story Content Goes Here Story Content
+					Goes Here Story Content Goes Here Story Content Goes Here
+				</p>
+				<div className="storygrid-col storygrid-col_3">
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+					<p>This space will hold a story with a vertical photo</p>
+				</div>
+			</div>
+
+			<div className="storygrid-col storygrid-col_1">
+				<p>
+					<h2>Story 4</h2>
+					Story Content Goes Here Story Content Goes Here Story Content Goes
+					Here Story Content Goes Here Story Content Goes Here Story Content
+					Goes Here Story Content Goes Here Story Content Goes Here
+				</p>
+				<div className="storygrid-row_1">
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+					<p>This space will hold a story with a horizontal photo</p>
+				</div>
+			</div>
+		</div>
 	</div>
 );
 
