37cd17d2d4e52ede16610d19a2c6e3bbbba83a9e
allthethings <learn2code@respectdesfonds.com>
2020-05-12 23:04:54 -0500

    adjusted placement of some routes so they are not in protected status

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -39,14 +39,14 @@ class App extends Component {
 						{/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
 						<Route exact path="/home" component={LandingPage} />
+						<ProtectedRoute exact path="/userpage" component={UserPage} />
 						<Route exact path="/works" component={HowItWorksPage} />
 						<Route exact path="/stories" component={StoriesPage} />
 						<Route exact path="/about" component={AboutPage} />
-						/>
 						{/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/user */}
+            Even though it seems like they are different pages, the user is always on localhost:3000/user
 						<ProtectedRoute exact path="/userpage" component={UserPage} />
 						{/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. 
