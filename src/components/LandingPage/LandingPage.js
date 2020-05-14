4487dc91b2ec6d8a79f8a6fc0f9f90c7e2064c89
allthethings <learn2code@respectdesfonds.com>
2020-05-14 17:52:43 -0500

    swapped out table for grid after correcting mapping so server data renders

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,7 +4,6 @@ import mapStoreToProps from "../../redux/mapStoreToProps";
 import "../pageview.css";
 import "../grids.css";
 import "../App/App.css";
-import { response } from "express";
 
 class LandingPage extends Component {
 	state = {
@@ -14,8 +13,7 @@ class LandingPage extends Component {
 	componentDidMount() {
 		// load up all information from the server
 		this.props.dispatch({
-			type: "SHOW_OFFERS",
-			payload: response.data,
+			type: "GET_OFFERS",
 		});
 	}
 
@@ -79,62 +77,29 @@ class LandingPage extends Component {
 							</div>
 						</form>
 					</div>
-
-					<div>
-						<h3>Current Offers</h3>
-						{/* FIX FOR RENDERING  */}
-						<ul>
-							<li>{this.props.store.agency}</li>
-						</ul>
-					</div>
-
-					{/* 
-					<Container maxWidth={false}>
-        <Grid container spacing={2}>
-          {limitedResults.map((item, index) => (
-            <Grid
-              item
-              xs={12}
-              sm={4}
-              md={3}
-              lg={2}
-            >
-              <MoviesListItem key={index} item={item} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container> */}
-					{/* 
-					<div className="datagrid">
-						<div className="datagrid-col datagrid-col_1">
-							<h4>Status</h4>
-							<div className="datagrid-row_1">
-								<p className="serverData">{this.props.offers.offer_status}</p>
-								<p className="serverData">Server Data Here</p>
-							</div>
-						</div>
-						<div className="datagrid-col datagrid-col_2">
-							<h4>Date Posted</h4>
-							<div className="datagrid-row_1">
-								<p className="serverData">Server Data Here</p>
-								<p className="serverData">Server Data Here</p>
-							</div>
-						</div>
-						<div className="datagrid-col datagrid-col_3">
-							<h4>Location</h4>
-							<div className="datagrid-row_1">
-								<p className="serverData">Server Data Here</p>
-								<p className="serverData">Server Data Here</p>
-							</div>
-						</div>
-						<div className="datagrid-col datagrid-col_4">
-							<h4>Items Offered</h4>
-							<div className="datagrid-row_1">
-								<p className="serverData">Server Data Here</p>
-								<p className="serverData">Server Data Here</p>
-							</div>
-						</div>
-					</div>*/}
+					<table>
+						<thead>
+							<h3>Current Offers</h3>
+						</thead>
+						<tbody>
+							<tr>
+								<th>Status</th>
+								<th>Date Posted</th>
+								<th>Location</th>
+								<th>Items Offered</th>
+							</tr>
+							{this.props.store.offersReducer.map((item, index) => (
+								<tr key={`index-${index}`}>
+									<td className="status">{item.offer_status}</td>
+									<td className="date">{item.submit_date}</td>
+									<td className="location">
+										{item.city}, {item.state}
+									</td>
+									<td className="description">{item.off_detail}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
 				</div>
 			</div>
 		);
